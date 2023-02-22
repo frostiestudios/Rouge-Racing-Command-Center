ed62ae5a7422db3f3f30ef5662f580744b8f0dfd
Dromo2106 <dromomod@gmail.com>
2023-02-21 16:41:30 -0800

    Create RougeRacing.py

---
+++ RougeRacing.py
@@ -0,0 +1,26 @@
+from appJar import gui
+import os
+import webbrowser
+import subprocess
+def rouge(btn):
+    if btn=="XAMPP":
+        print("opening XAMPP")
+        os.startfile("C:/XAMPP/xampp-control.exe")
+    if btn=="Lounge Control":
+        print("Lounge Control Is Running")
+        os.startfile("C:/LoungeControl/LoungeControl-Server/LoungeControl-Server.exe")
+    if btn=="Remote Launcher":
+        print("Starting Remote Launcher")
+        webbrowser.open_new_tab("https://192.168.1.12:6001/ACLauncherControl")
+        print("https://192.168.1.12:6001/ACLauncherControl")
+    if btn=="MultiServer":
+        os.open("C:\LoungeControl\LoungeControl-ACMultiServer\LC-AC-MultiServer.exe")
+    if btn=="Leaderboard Control":
+        webbrowser.open_new_tab("https://192.168.1.12:6001/RemoteViewSettings")
+        print("https://192.168.1.12:6001/RemoteViewSettings")
+
+
+app = gui("Command Center")
+app.addButtons(["XAMPP","Lounge Control","MultiServer"],rouge)
+app.addButtons(["Remote Launcher","Leaderboard Control"],rouge)
+app.go()
