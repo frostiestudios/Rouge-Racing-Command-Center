62669920ccc14ef2c1b481c4047665467ec29108
thememetsar <mattwiner04@gmail.com>
2023-02-21 17:03:30 -0800

    Update RougeRacing.py

--- RougeRacing.py
+++ RougeRacing.py
@@ -2,36 +2,44 @@ from appJar import gui
 import os
 import webbrowser
 import subprocess
-def rouge(btn):
-    if btn=="XAMPP":
-        print("opening XAMPP")
-        os.startfile("C:/XAMPP/xampp-control.exe")
-        subprocess.call(['C:/XAMPP/xampp-control.exe', 'startapache', 'startmysql'])
-        app.infoBox("Success", "Apache and MySQL servers started!")
-    if btn=="Lounge Control":
-        print("Lounge Control Is Running")
-        os.startfile("C:/LoungeControl/LoungeControl-Server/LoungeControl-Server.exe")
-    if btn=="Remote Launcher":
-        print("Starting Remote Launcher")
-        webbrowser.open_new_tab("https://192.168.1.12:6001/ACLauncherControl")
-        print("https://192.168.1.12:6001/ACLauncherControl")
-    if btn=="MultiServer":
-        os.open("C:\LoungeControl\LoungeControl-ACMultiServer\LC-AC-MultiServer.exe")
-    if btn=="Leaderboard Control":
-        webbrowser.open_new_tab("https://192.168.1.12:6001/RemoteViewSettings")
-        print("https://192.168.1.12:6001/RemoteViewSettings")
+import threading
 def xampp(btn):
     if btn=="Start":
         print("opening XAMPP")
         os.startfile("C:/XAMPP/xampp-control.exe")
+        threading.Thread(target=xampp).start() 
         subprocess.call(['C:/XAMPP/xampp-control.exe', 'startapache', 'startmysql'])
         app.infoBox("Success", "Apache and MySQL servers started!")
-
+def lc(btn):
+    print("Lounge Control")
+    if btn=="LCOpen":
+        print("Lounge Control Is Running")
+        os.startfile("C:/LoungeControl/LoungeControl-Server/LoungeControl-Server.exe")
+    if btn=="LCStop":
+        print("Lounge Control Is Closed")
+        os.stopfile("C:/LoungeControl/LoungeControl-Server/LoungeControl-Server.exe")
+    
+def ms(btn):    
+    print("MultiServer")
+    if btn=="MSOpen":
+        os.open("C:\LoungeControl\LoungeControl-ACMultiServer\LC-AC-MultiServer.exe")
 app = gui("Command Center")
 app.addButtons(["XAMPP","Lounge Control","MultiServer"],rouge)
-app.addButtons(["Remote Launcher","Leaderboard Control"],rouge)
 
-app.startLabelFrame("Step 1")
-app.addButtons(["Start","Stop"],xampp)
+
+app.startLabelFrame("XAMPP",1,1)
+app.addButtons(["XAMPPStart","XAMPPStop"],xampp)
+app.stopLabelFrame()
+
+app.startLabelFrame("Lounge Control",1,2)
+app.addButtons(["LCOpen","LCStop"],lc)
+app.stopLabelFrame()
+
+app.startLabelFrame("MultiServer",1,3)
+app.addButtons(["MSOpen","MSStop"],ms)
+app.stopLabelFrame()
+
+app.startLabelFrame("Lounge Control Settigns",2,1,3)
+app.addButtons(["Remote Launcher","Leaderboard Control"],rouge)
 app.stopLabelFrame()
 app.go()
