fc6045a2b6df31b22795e34a691bea7b1769686a
thememetsar <mattwiner04@gmail.com>
2023-02-21 16:47:48 -0800

    Update RougeRacing.py

--- RougeRacing.py
+++ RougeRacing.py
@@ -6,6 +6,8 @@ def rouge(btn):
     if btn=="XAMPP":
         print("opening XAMPP")
         os.startfile("C:/XAMPP/xampp-control.exe")
+        subprocess.call(['C:/XAMPP/xampp-control.exe', 'startapache', 'startmysql'])
+        app.infoBox("Success", "Apache and MySQL servers started!")
     if btn=="Lounge Control":
         print("Lounge Control Is Running")
         os.startfile("C:/LoungeControl/LoungeControl-Server/LoungeControl-Server.exe")
@@ -18,9 +20,18 @@ def rouge(btn):
     if btn=="Leaderboard Control":
         webbrowser.open_new_tab("https://192.168.1.12:6001/RemoteViewSettings")
         print("https://192.168.1.12:6001/RemoteViewSettings")
-
+def xampp(btn):
+    if btn=="Start":
+        print("opening XAMPP")
+        os.startfile("C:/XAMPP/xampp-control.exe")
+        subprocess.call(['C:/XAMPP/xampp-control.exe', 'startapache', 'startmysql'])
+        app.infoBox("Success", "Apache and MySQL servers started!")
 
 app = gui("Command Center")
 app.addButtons(["XAMPP","Lounge Control","MultiServer"],rouge)
 app.addButtons(["Remote Launcher","Leaderboard Control"],rouge)
+
+app.startLabelFrame("Step 1")
+app.addButtons(["Start","Stop"],xampp)
+app.stopLabelFrame()
 app.go()
